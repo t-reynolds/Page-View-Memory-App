e0ab825f5e1c4974d0cfb41df11d7f1ed0cb9ce7
t-reynolds <timmy12011@gmail.com>
2016-07-21 13:57:17 -0400

    Initial Commit

---
+++ Page View Controller Alpha/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Page View Controller Alpha
+//
+//  Created by Robert Reynolds on 7/21/16.
+//  Copyright © 2016 Robert Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ Page View Controller AlphaTests/Page_View_Controller_AlphaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Page_View_Controller_AlphaTests.swift
+//  Page View Controller AlphaTests
+//
+//  Created by Robert Reynolds on 7/21/16.
+//  Copyright © 2016 Robert Reynolds. All rights reserved.
+//
+
+import XCTest
+@testable import Page_View_Controller_Alpha
+
+class Page_View_Controller_AlphaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
